06040dcaef84a8d2f3a87fb0c80b30b75434305f
Justin Stockton <jstockton@devis.com>
2015-03-04 14:30:28 -0800

    Refactor some of the duplicate functions into a utils modules

--- example/index.js
+++ example/index.js
@@ -2,18 +2,27 @@
 
 // Import the module
 var fs = require('fs'),
-    convert = require('../lib/xunit.js');
+    convert = require('../lib/html.js');
 
-fs.readFile('./test/result.json', function (err, data) {
+fs.readFile('./test/result-url.json', function (err, data) {
     if (err) {
         throw err;
     }
 
+    console.log(JSON.parse(data.toString()));
+    
     convert(JSON.parse(data.toString()), function(err, result) {
         if (err) {
             console.error(err);
         } else {
             console.log(result);
+
+            fs.writeFile('test.html', result, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('Tenon Response written to: test.html');
+            });
         }
     });
 });
