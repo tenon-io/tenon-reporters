69b28eb4eb31aa595ee2a0f8f63f98e6e19d0db3
Marko Milanović <marko.macmilan@gmail.com>
2022-02-23 15:01:02 +0100

    Refactor tests.

--- test/tenon-utils_test.js
+++ test/tenon-utils_test.js
@@ -8,7 +8,7 @@ describe('Tenon Utils', function() {
 
     describe('updateBooleanValues()', function() {
 
-        var json;
+        let json;
 
         beforeEach(function() {
             json = {
@@ -20,27 +20,27 @@ describe('Tenon Utils', function() {
         });
 
         it('must convert request.fragment to a boolean', function() {
-            var result = utils.updateBooleanValues(json);
+            const result = utils.updateBooleanValues(json);
             expect(result.request.fragment).to.be.true();
         });
 
         it('must convert request.store to a boolean', function() {
-            var result = utils.updateBooleanValues(json);
+            const result = utils.updateBooleanValues(json);
             expect(result.request.store).to.be.true();
         });
     });
 
     describe('bytesToSize()', function() {
         it('must convert value to Kb', function() {
-            expect(utils.bytesToSize(0)).to.be('0 Byte');
-            expect(utils.bytesToSize(1)).to.be('1.00 Bytes');
-            expect(utils.bytesToSize(10)).to.be('10.0 Bytes');
-            expect(utils.bytesToSize(100)).to.be('100 Bytes');
-            expect(utils.bytesToSize(1000)).to.be('1.00 KB');
-            expect(utils.bytesToSize(10000)).to.be('10.0 KB');
-            expect(utils.bytesToSize(100000)).to.be('100 KB');
-            expect(utils.bytesToSize(1000000)).to.be('1.00 MB');
-            expect(utils.bytesToSize(10000000)).to.be('10.0 MB');
+            expect(utils.bytesToSize(0)).to.be.equal('0 Byte');
+            expect(utils.bytesToSize(1)).to.be.equal('1.00 Bytes');
+            expect(utils.bytesToSize(10)).to.be.equal('10.0 Bytes');
+            expect(utils.bytesToSize(100)).to.be.equal('100 Bytes');
+            expect(utils.bytesToSize(1000)).to.be.equal('1.00 KB');
+            expect(utils.bytesToSize(10000)).to.be.equal('10.0 KB');
+            expect(utils.bytesToSize(100000)).to.be.equal('100 KB');
+            expect(utils.bytesToSize(1000000)).to.be.equal('1.00 MB');
+            expect(utils.bytesToSize(10000000)).to.be.equal('10.0 MB');
         });
     });
 });
